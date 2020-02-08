9b3d794be5427ddefb255d22944538ae82ab8baf
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-02-08 11:16:14 -0800

    added in the user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/users', to: "users#index"
   get 'comments/new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
