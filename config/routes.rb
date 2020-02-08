85d8369706345f9bc21fc45f667aa4900853e06f
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-02-08 13:24:52 -0800

    editing authentication

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'welcome', to: 'sessions#welcome'
   get 'sessions/login'
   get 'sessions/welcome'
+  get 'authorized', to: 'session#page_requires_login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
